65bb49cf12dafb00dd07188affc8a0f9bc5d9466
GaoLing <1404919041@qq.com>
2017-09-05 17:58:54 +0800

    add basic file

--- explore.cpp
+++ explore.cpp
@@ -1,1 +1,16 @@
+/****************************************
+*Filename:explore.cpp
+*CreateTime:2017/9/5 Tue 17:50:40
+*CreateBy:GaoLing
+*Email:zxgaoling@gamil.com/1404919041@qq.com
+*CopyRight:whatever
+****************************************/
 
+
+#include "header.h"
+
+int main(void)
+{
+
+  return 0;
+}
---
+++ header.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+#endif<`0`>
+
+
